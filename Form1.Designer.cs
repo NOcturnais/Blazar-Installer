60b45f39fb76bdc3641a4a9e5325ca508bfd54da
NOcturnais <NOcturnais1412@gmail.com>
2020-03-18 19:28:30 +0100

    First
    add project files

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Blazar_Installer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.button1.Location = new System.Drawing.Point(166, 484);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Install";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Menu;
+            this.button2.Location = new System.Drawing.Point(630, 484);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Uninstall";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(115, 108);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(669, 353);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Location = new System.Drawing.Point(199, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(500, 51);
+            this.panel1.TabIndex = 3;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_mouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.Location = new System.Drawing.Point(-5, -1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1241, 11);
+            this.panel2.TabIndex = 4;
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseDown);
+            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseMove);
+            this.panel2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(916, 691);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "BlazarInstaller";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.IO.Compression;
+
+namespace Blazar_Installer
+{
+    public partial class Form1 : Form
+    {
+
+        private bool mouseDown;
+        private Point lastLocation;
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            richTextBox1.ReadOnly = true;
+            button1.FlatStyle = FlatStyle.Flat;
+            button2.FlatStyle = FlatStyle.Flat;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            String appdata = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            String mcDir = Path.Combine(appdata, ".minecraft/");
+            String modDir = Path.Combine(mcDir, "mods/");
+            String versionsDir = Path.Combine(mcDir, "versions/paladium/");
+            String libsDir = Path.Combine(mcDir, "libraries/net/minecraftforge/forge/paladium");
+
+            if (Directory.Exists(modDir))
+            {
+
+                DirectoryInfo di = new DirectoryInfo(modDir);
+
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+
+            }
+
+            if (Directory.Exists(versionsDir))
+            {
+
+                DirectoryInfo di = new DirectoryInfo(versionsDir);
+
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+
+                Directory.Delete(versionsDir);
+
+            }
+
+            if (Directory.Exists(libsDir))
+            {
+
+                DirectoryInfo di = new DirectoryInfo(libsDir);
+
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+
+                Directory.Delete(libsDir);
+
+            }
+
+            MessageBox.Show("Blazar a bien été désinstallé !", "Blazar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void addInfoRTB(String info)
+        {
+
+            richTextBox1.Text = richTextBox1.Text + info + "\n";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            button1.FlatStyle = FlatStyle.Flat;
+
+            String cDir = Directory.GetCurrentDirectory();
+            String appdata = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            String cLibs = Path.Combine(cDir, "libs.zip");
+            String cMods = Path.Combine(cDir, "mods.zip");
+            String cVersions = Path.Combine(cDir, "versions.zip");
+          
+            String mcDir = Path.Combine(appdata, ".minecraft/");
+            String modDir = Path.Combine(mcDir, "mods/");
+            String versionsDir = Path.Combine(mcDir, "versions/");
+            String libsDir = Path.Combine(mcDir, "libraries/net/minecraftforge/forge/");
+
+            String palaLibsZip = Path.Combine(libsDir, "libs.zip");
+            String palaVersionZip = Path.Combine(versionsDir, "versions.zip");
+            String palaModsZip = Path.Combine(modDir, "mods.zip");
+
+            addInfoRTB("Initializing...");
+
+            //----------------------------------------------------------------------------------------------------------------------//
+
+            if (File.Exists(palaLibsZip)){File.Delete(palaLibsZip);}
+            if (File.Exists(palaVersionZip)) { File.Delete(palaVersionZip); }
+            if (File.Exists(palaModsZip)) { File.Delete(palaModsZip); }
+
+            //----------------------------------------------------------------------------------------------------------------------//
+
+            WebClient webClient = new WebClient();
+            String downloadString = webClient.DownloadString("https://pastebin.com/raw/PBXwn0Pc");
+
+            //----------------------------------------------------------------------------------------------------------------------//
+
+            string remoteUri = downloadString;
+            string fileName = "libs.zip", myStringWebResource = null;
+            WebClient myWebClient = new WebClient();
+            myStringWebResource = remoteUri + fileName;
+            myWebClient.DownloadFile(myStringWebResource, fileName);
+
+            addInfoRTB("Downloading libs...");
+
+            File.Move(cLibs, Path.Combine(libsDir, "libs.zip"));
+
+            addInfoRTB("Moveing libs...");
+
+            Directory.CreateDirectory(Path.Combine(libsDir, "paladium/"));
+            addInfoRTB("Creating libs dir...");
+            ZipFile.ExtractToDirectory(palaLibsZip, Path.Combine(libsDir, "paladium/"));
+            addInfoRTB("Extracting...");
+            File.Delete(palaLibsZip);
+            addInfoRTB("Deleting libs zip...");
+
+            //----------------------------------------------------------------------------------------------------------------------//
+
+            string remoteUrid = downloadString;
+            string fileNamed = "mods.zip", myStringWebResourced = null;
+            WebClient myWebClientd = new WebClient();
+            myStringWebResourced = remoteUrid + fileNamed;
+            myWebClientd.DownloadFile(myStringWebResourced, fileNamed);
+            addInfoRTB("Downloading mods...");
+
+            if (File.Exists(cMods))
+            {
+
+                File.Move(cMods, Path.Combine(modDir, "mods.zip"));
+                addInfoRTB("Moving mods...");
+
+            }
+
+            ZipFile.ExtractToDirectory(palaModsZip, modDir);
+            addInfoRTB("Extracting...");
+            File.Delete(palaModsZip);
+            addInfoRTB("Deleting mods zip...");
+
+            //----------------------------------------------------------------------------------------------------------------------//
+
+            string remoteUrit = downloadString;
+            string fileNamet = "versions.zip", myStringWebResourcet = null;
+            WebClient myWebClientt = new WebClient();
+            myStringWebResourcet = remoteUrit + fileNamet;
+            myWebClientt.DownloadFile(myStringWebResourcet, fileNamet);
+            addInfoRTB("Downloading vers...");
+
+            if (File.Exists(cVersions))
+            {
+
+                File.Move(cVersions, Path.Combine(versionsDir, "versions.zip"));
+                addInfoRTB("Moving vers...");
+
+            }
+
+            ZipFile.ExtractToDirectory(palaVersionZip, Path.Combine(versionsDir, "paladium/"));
+            addInfoRTB("Creating vers dir...");
+            File.Delete(palaVersionZip);
+            addInfoRTB("Deleting vers zip...");
+
+            addInfoRTB("Finish !");
+
+            MessageBox.Show("Blazar a été installé !", "Blazar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void panel1_mouseDown(object sender, MouseEventArgs e)
+        {
+
+            mouseDown = true;
+            lastLocation = e.Location;
+
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if (mouseDown)
+            {
+                this.Location = new Point(
+                    (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+                this.Update();
+            }
+
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+            mouseDown = false;
+
+        }
+
+        private void panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            mouseDown = true;
+            lastLocation = e.Location;
+
+        }
+
+        private void panel2_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if (mouseDown)
+            {
+                this.Location = new Point(
+                    (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+                this.Update();
+            }
+
+        }
+
+        private void panel2_MouseUp(object sender, MouseEventArgs e)
+        {
+
+            mouseDown = false;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            System.Net.ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+            WebClient webClient = new WebClient();
+            if (!webClient.DownloadString("https://pastebin.com/raw/LFvjNEEP").Contains("1.5"))
+            {
+
+                MessageBox.Show("Une mise à jour est disponible, allez sur le discord pour la télécharger !", "OUAH", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+        }
+    }
+}
